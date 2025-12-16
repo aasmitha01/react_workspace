f98d710068ff3e16ad03b13d5815db4a6dab0792
aasmitha01 <tommandruaasmitha@gmail.com>
2025-12-16 12:49:01 +0530

    Learing React

---
+++ Day-2/code/src/App.jsx
@@ -0,0 +1,10 @@
+import FirstComponent from "./FirstComponent";
+function App() {
+  return (
+    <>
+      <FirstComponent />
+    </>
+  )
+}
+
+export default App
---
+++ Day-2/code/src/FirstComponent.jsx
@@ -0,0 +1,7 @@
+function FirstComponent() {
+  return (
+    <h2>This is my first react component</h2>
+  );
+}
+
+export default FirstComponent;
