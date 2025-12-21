bef2ec96d7cea32e1cb6b8ba4489101473288532
aasmitha01 <tommandruaasmitha@gmail.com>
2025-12-21 13:27:26 +0530

    Video player using useRef With controls

---
+++ Day-6.1/code/src/App.jsx
@@ -0,0 +1,5 @@
+import VideoPlayer from "./VideoPlayer";
+function App(){
+  return <VideoPlayer/>
+}
+export default App;
---
+++ Day-6.1/code/src/VideoPlayer.jsx
@@ -0,0 +1,31 @@
+import { useRef } from "react";
+
+function VideoPlayer() {
+  const videoRef = useRef(null);
+
+  return (
+    <div>
+      <video
+        ref={videoRef}
+        width="400"
+        src="https://www.w3schools.com/html/mov_bbb.mp4"
+      ></video>
+
+      <div>
+        <button onClick={() => videoRef.current.play()}>▶️ Play</button>
+
+        <button onClick={() => videoRef.current.pause()}>⏸ Pause</button>
+
+        <button onClick={() => (videoRef.current.currentTime += 5)}>
+          ⏩ Forward
+        </button>
+
+        <button onClick={() => (videoRef.current.currentTime -= 5)}>
+          ⏪ Rewind
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default VideoPlayer;
